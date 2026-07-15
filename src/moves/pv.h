17c4778958f58ef352461364019abbe7a0f090fd
sasha <tastakov01@gmail.com>
2026-07-15 11:01:02 +0300

    remove constructors in pv

--- src/moves/pv.h
+++ src/moves/pv.h
@@ -7,12 +7,6 @@ struct PrincipalVariation {
     MoveType moves[30];
     int length = 0;
 
-    PrincipalVariation() = default;
-    PrincipalVariation(const PrincipalVariation& other) {
-        length = other.length;
-        memcpy(moves, other.moves, length * sizeof(MoveType));
-    }
-
     void Set(const MoveType& move, const PrincipalVariation& rest) {
         moves[0] = move;
         memcpy(moves + 1, rest.moves, rest.length * sizeof(MoveType));
